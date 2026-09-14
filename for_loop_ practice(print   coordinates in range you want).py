746460dba6b90bfb8880cd176556fad321a685ac
amir <amirmohamadhosseini6@gmail.com>
2026-09-14 10:25:32 +0330

    Add files via upload

---
+++ for_loop_ practice(print   coordinates in range you want).py
@@ -0,0 +1,9 @@
+x_range = int(input("x range:"))
+y_range = int(input("y range:"))
+for x in range(x_range):
+    for y in range(y_range):
+        print(f'({x},{y})')
+exit = input("press enter to exit ")
+
+
+
